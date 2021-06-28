1ffe3bdff0564182752bb324b62f8e914a361685
Wenfang Du <duwenfangcn@163.com>
2021-06-28 10:11:35 +0800

    added repro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,25 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return () => <div></div>
+    return () => (
+      <ElDropdown>
+        {{
+          default: () => (
+            <span class='el-dropdown-link'>
+              下拉菜单<i class='el-icon-arrow-down el-icon--right'></i>
+            </span>
+          ),
+          dropdown: () => (
+            <ElDropdownMenu>
+              <ElDropdownItem>黄金糕</ElDropdownItem>
+              <ElDropdownItem>狮子头</ElDropdownItem>
+              <ElDropdownItem>螺蛳粉</ElDropdownItem>
+              <ElDropdownItem disabled>双皮奶</ElDropdownItem>
+              <ElDropdownItem divided>蚵仔煎</ElDropdownItem>
+            </ElDropdownMenu>
+          ),
+        }}
+      </ElDropdown>
+    )
   },
 })
