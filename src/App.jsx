86599138b226d76009cc0d8646187699569981b9
Wenfang Du <no_reply_mail@163.com>
2021-07-28 11:35:30 +0800

    added repro

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { defineComponent } from 'vue'
 
 export default defineComponent({
   setup() {
-    return () => <div></div>
+    return () => (
+      <ElTable>
+        <ElTableColumn>
+          {{
+            // 👇 default slot is called even if there's no data
+            default: () => console.log('called'),
+          }}
+        </ElTableColumn>
+      </ElTable>
+    )
   },
 })
