dc8cf3563b426df58fa0f0e9928d980b5bea8418
Wenfang Du <no_reply_mail@163.com>
2021-08-06 22:19:44 +0800

    added repro

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,18 @@
 import { defineComponent } from 'vue'
 
+const Comp = defineComponent({
+  setup(props, { slots }) {
+    return () => (
+      <div>
+        Comp
+        {slots.default()}
+      </div>
+    )
+  },
+})
+
 export default defineComponent({
   setup() {
-    return () => <div></div>
+    return () => <Comp>{{ default: () => 1, ...{} }}</Comp>
   },
 })
